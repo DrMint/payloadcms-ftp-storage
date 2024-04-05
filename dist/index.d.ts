cc3534b902b6afd6052b158585bb2e75ec76ca35
DrMint <29893320+DrMint@users.noreply.github.com>
2024-04-05 23:09:54 +0200

    Updated to 1.0.1 (fixed missing mock.js in build)

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Adapter } from "@payloadcms/plugin-cloud-storage/dist/types";
-export interface FTPAdapterConfig {
-    host: string;
-    user: string;
-    password: string;
-    secure: boolean;
-    endpoint: string;
-}
-export declare const ftpAdapter: ({ endpoint, host, password, secure, user }: FTPAdapterConfig) => Adapter;
